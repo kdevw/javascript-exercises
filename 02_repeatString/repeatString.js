910509ac1ec3b4958c0a17020e6eb3a936ca8d54
Kyle Wilson <kw@kyledwilson.com>
2022-10-03 14:14:34 -0400

    Wrote function and passed 7 tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(the_string, times_to_repeat) {
+	
+	if ( times_to_repeat < 0 ) {
+		return "ERROR";
+	}
 
+	let final_string = '';
+
+	for ( i = 0; i < times_to_repeat; i++ ) {
+		final_string = final_string + the_string;
+	}
+
+	return final_string;
 };
 
 // Do not edit below this line
