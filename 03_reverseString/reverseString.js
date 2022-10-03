d6694de46352e30df690d13686fed6ebeb93f5e0
Kyle Wilson <kw@kyledwilson.com>
2022-10-03 14:23:49 -0400

    Wrote function to reverse a string and passed 4 tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string_to_reverse) {
+
+	if ( typeof string_to_reverse != 'string' ) {
+		return "ERROR";
+	}
+
+	let exploded_string = string_to_reverse.split('');
+
+	exploded_string.reverse();
+
+	let final_string = exploded_string.join('');
+
+	return final_string;
 
 };
 
